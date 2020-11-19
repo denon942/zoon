70ef8c2f5a8a21d7b11250fb1c85b8161f52e6f8
Hyaneru Satoki <park.sooneul@gmail.com>
2020-11-19 18:12:07 +0900

    チャット画面色々いじる

--- src/views/chat.vue
+++ src/views/chat.vue
@@ -1,10 +1,21 @@
 <template>
-    <v-container class="ma-0 pa-0" >
-        <v-layout class="ma-0 pa-0">
-            <v-flex xs12 lg12 >
-                <v-btn @click="logout">ログアウト</v-btn>
-            </v-flex>
-        </v-layout>
+    <v-container class="ma-0 pa-0" fluid>
+        <v-row justify="center" class="ma-0 pa-0 mt-3">
+            <v-col cols="7" class="ma-0 pa-0">
+                <v-card class="indigo lighten-4 white--text" height="650" tile>
+                    VIDEO
+                </v-card>
+            </v-col>
+            <v-col cols="4" class="ma-0 pa-0">
+                <v-card class="blue" height="650" tile elevation="2">
+                    <v-btn
+                        elevation="2"
+                        text
+                        class="red"
+                        >CHAT</v-btn>
+                </v-card>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 <script>
@@ -18,9 +29,6 @@ export default {
         onResize () {
             this.x = window.innerWidth
             this.y = window.innerHeight
-        },
-        logout:function(){
-            this.$store.commit('logout')
         },
     },
     created() {
