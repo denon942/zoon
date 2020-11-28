d81f988e5684669a923f0689b4d073f00d7465e8
片山翼 <denon38msn@gmail.com>
2020-11-28 22:35:59 +0900

    katayama_indent

--- src/views/chat.vue
+++ src/views/chat.vue
@@ -1,152 +1,118 @@
 <template>
-    <v-container class="ma-0 pa-0" fluid>
-        <v-row justify="center" class="ma-0 pa-0 mt-3">
-            <!-- ビデオ表示領域 -->
-            <v-col cols="7" class="ma-0 pa-0">
-                <v-card height="650" tile elevation="1" class="grey">
-                    VIDEO
-                </v-card>
-            </v-col>
-            
-            <!-- チャット表示領域 -->
-            <v-col cols="4" class="ma-1 pa-0">
-                <!-- チャット履歴 -->
-                <v-card 
-                    class="white display-1 overflow-y-auto" max-height="585"
-                    tile elevation="0"
-                    min-height="585">
-                    <v-card-text
-                        v-for="(item,index) in chat"
-                        :key="index"
-                        :index="index"
-                        class="black--text"
-                    >
-                        {{ item.name }}:
-                        <v-card-text
-                            class="title pt-0 ma-0">
-                            {{item.content}}
-                        </v-card-text>
+<v-container class="ma-0 pa-0" fluid>
+    <v-row justify="center" class="ma-0 pa-0 mt-3">
+        <!-- ビデオ表示領域 -->
+        <v-col cols="7" class="ma-0 pa-0">
+            <v-card height="650" tile elevation="1" class="grey">
+                VIDEO
+            </v-card>
+        </v-col>
+
+        <!-- チャット表示領域 -->
+        <v-col cols="4" class="ma-1 pa-0">
+            <!-- チャット履歴 -->
+            <v-card class="white display-1 overflow-y-auto" max-height="585" tile elevation="0" min-height="585">
+                <v-card-text v-for="(item,index) in chat" :key="index" :index="index" class="black--text">
+                    {{ item.name }}:
+                    <v-card-text class="title pt-0 ma-0">
+                        {{item.content}}
                     </v-card-text>
-                </v-card>
+                </v-card-text>
+            </v-card>
 
-                <!-- 入力領域 -->
-                <v-row justify="center" class="ma-0 pa-0 mt-2">
-                    <!-- テキストボックス -->
-                    <v-col cols="9" class="ma-0 pa-0" elevation="0">
-                        <v-form>
-                            <v-container class="pa-0 ma-0">
-                                <v-text-field
-                                    v-model="coment"
-                                    outlined
-                                    clearable
-                                    label="メッセージを入力"
-                                    type="text"
-                                >
-                                </v-text-field>
-                            </v-container>
-                        </v-form>
-                    </v-col>
+            <!-- 入力領域 -->
+            <v-row justify="center" class="ma-0 pa-0 mt-2">
+                <!-- テキストボックス -->
+                <v-col cols="9" class="ma-0 pa-0" elevation="0">
+                    <v-form>
+                        <v-container class="pa-0 ma-0">
+                            <v-text-field v-model="coment" outlined clearable label="メッセージを入力" type="text">
+                            </v-text-field>
+                        </v-container>
+                    </v-form>
+                </v-col>
 
-                    <!-- 別画面で表示ボタン -->
-                    <v-col cols="auto" class="ma-0 pa-0 pl-2">
-                        <v-btn 
-                            height="56"
-                            color="primary"
-                            dark
-                            @click="send"
-                        >
-                            <v-icon>mdi-send</v-icon>
-                        </v-btn>
-                    </v-col>
-                </v-row>
-            </v-col>
-        </v-row>
-        <v-footer
-            dark
-            padless
-        >
-            <v-card
-                class="flex"
-                flat
-                tile
-                min-height="122"
-            >
+                <!-- 別画面で表示ボタン -->
+                <v-col cols="auto" class="ma-0 pa-0 pl-2">
+                    <v-btn height="56" color="primary" dark @click="send">
+                        <v-icon>mdi-send</v-icon>
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-col>
+    </v-row>
+    <v-footer dark padless>
+        <v-card class="flex" flat tile min-height="122">
             <v-card-title class="teal">
                 <strong class="subheading">Zoon!</strong>
 
                 <v-spacer></v-spacer>
 
-                <v-btn
-                    v-for="icon in icons"
-                    :key="icon"
-                    class="mx-4"
-                    dark
-                    icon
-                    to="/mypage"
-                >
-                <v-icon size="24px">
-                    {{ icon }}
-                </v-icon>
+                <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon to="/mypage">
+                    <v-icon size="24px">
+                        {{ icon }}
+                    </v-icon>
                 </v-btn>
             </v-card-title>
 
             <v-card-text class="py-2 white--text text-center">
                 {{ new Date().getFullYear() }} — <strong>Zoon</strong>
             </v-card-text>
-            </v-card>
-        </v-footer>
-    </v-container>
+        </v-card>
+    </v-footer>
+</v-container>
 </template>
+
 <script>
 import firebase from 'firebase'
 export default {
-    data(){
-        return{
-            overlay:false,
-            chat:[],
-            chat_ire:[],
+    data() {
+        return {
+            overlay: false,
+            chat: [],
+            chat_ire: [],
             opacity: 0.4,
-            coment:"",
-            name:"",
+            coment: "",
+            name: "",
             icons: [
                 'mdi-home',
             ],
         }
     },
     methods: {
-        onResize () {
+        onResize() {
             this.x = window.innerWidth
             this.y = window.innerHeight
         },
-        change:function(){
+        change: function () {
             this.overlay = !this.overlay
         },
-        getChats(){
+        getChats() {
             firebase.firestore().collection('room').doc('001').collection('comments').orderBy('createdAt', 'asc').get().then(async snapshot => {
-                    await snapshot.forEach(doc => {
+                await snapshot.forEach(doc => {
                     //contentは要素
                     //pushは配列データそのもの
                     // this.allData.push(doc.data().content)
                     this.chat_ire.push({
-                        content:doc.data().content,
-                        name:doc.data().name
+                        content: doc.data().content,
+                        name: doc.data().name
                     })
                 })
                 this.chat = this.chat_ire
                 this.chat_ire = []
             })
         },
-        send:function(){
+        send: function () {
             this.name = this.user_name
             firebase.firestore().collection('room').doc('001').collection('comments').add({
                 content: this.coment,
                 createdAt: new Date(),
-                name:this.name
+                name: this.name
             })
 
             this.chat.push({
-                content:this.coment,
-                name:this.name
+                content: this.coment,
+                name: this.name
             })
             this.coment = ""
         }
@@ -156,16 +122,16 @@ export default {
             this.getChats()
         })
     },
-    computed:{
-        user_id:{
-            get(){
+    computed: {
+        user_id: {
+            get() {
                 return this.$store.getters.user_id
             },
-            set(value){
-                return this.$store.commit('set_user_id',value)
+            set(value) {
+                return this.$store.commit('set_user_id', value)
             }
         },
-        user_name(){
+        user_name() {
             return this.$store.getters.user_fname
         },
     },
--- src/views/mypage.vue
+++ src/views/mypage.vue
@@ -1,131 +1,103 @@
 <template>
-    <content class="ma-0 pa-0">
-        <v-container class="ma-0 pa-0" fluid>
-            <v-layout class="ma-0 pa-0" wrap>
-                <v-flex xs12 lg12>
-                    <v-row class="ma-0 pa-0" style="background-color: #F6F6F6">
-                        <v-col cols="12" lg="12">
-                            <v-row justify="center" class="ma-0 pa-0">
-                                <v-col cols="12" lg="12">
-                                    <v-card class="ma-0 pa-0" elevation="0" style="background-color: #F6F6F6">
-                                        <p class=" justify-center font-weight-light" :class="size">ようこそ</p>
-                                        <v-row justify="center" class="ma-0 pa-0">
-                                            <p class="mt-12 display-3" :class="size">
-                                                {{user_fname}}
-                                                <span class="display-1">さん</span>
-                                            </p>
-                                        </v-row>
-                                    </v-card>
-                                </v-col>
-                            </v-row>
-                        </v-col>
-                    </v-row>
-                    <v-row justify="center" class="ma-0 pa-0">
-                        <v-card class="ma-0 pa-0" elevation="0" >
-                            <v-row justify="end" align="end" class="ma-0 pa-0 mt-2">
-                                <v-col>
-                                    <v-btn
-                                        outlined
-                                        large
-                                        color="primary"
-                                        to="chat"
-                                        class="mb-5"
-                                        width="150"
-                                    >
+<content class="ma-0 pa-0">
+    <v-container class="ma-0 pa-0" fluid>
+        <v-layout class="ma-0 pa-0" wrap>
+            <v-flex xs12 lg12>
+                <v-row class="ma-0 pa-0" style="background-color: #F6F6F6">
+                    <v-col cols="12" lg="12">
+                        <v-row justify="center" class="ma-0 pa-0">
+                            <v-col cols="12" lg="12">
+                                <v-card class="ma-0 pa-0" elevation="0" style="background-color: #F6F6F6">
+                                    <p class=" justify-center font-weight-light" :class="size">ようこそ</p>
+                                    <v-row justify="center" class="ma-0 pa-0">
+                                        <p class="mt-12 display-3" :class="size">
+                                            {{user_fname}}
+                                            <span class="display-1">さん</span>
+                                        </p>
+                                    </v-row>
+                                </v-card>
+                            </v-col>
+                        </v-row>
+                    </v-col>
+                </v-row>
+                <v-row justify="center" class="ma-0 pa-0">
+                    <v-card class="ma-0 pa-0" elevation="0">
+                        <v-row justify="end" align="end" class="ma-0 pa-0 mt-2">
+                            <v-col>
+                                <v-btn outlined large color="primary" to="chat" class="mb-5" width="150">
                                     <span style="font-size:18px">
                                         チャット
                                     </span>
-                                    </v-btn>
-                                </v-col>
-                            </v-row>
-                        </v-card>
-                    </v-row>
-                    <v-row justify="center" class="ma-0 pa-0">
-                        <v-card class="ma-0 pa-0" elevation="0" min-height="375">
-                            <v-row justify="end" align="end" class="ma-0 pa-0">
-                                <v-col>
-                                    <v-btn
-                                        outlined
-                                        large
-                                        color="error"
-                                        @click="logout"
-                                        class="mb-5"
-                                        width="150"
-                                    >
+                                </v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </v-row>
+                <v-row justify="center" class="ma-0 pa-0">
+                    <v-card class="ma-0 pa-0" elevation="0" min-height="375">
+                        <v-row justify="end" align="end" class="ma-0 pa-0">
+                            <v-col>
+                                <v-btn outlined large color="error" @click="logout" class="mb-5" width="150">
                                     <span style="font-size:18px">
                                         ログアウト
                                     </span>
-                                    </v-btn>
-                                </v-col>
-                            </v-row>
-                        </v-card>
-                    </v-row>
-                    <v-footer
-                        dark
-                        padless
-                    >
-                        <v-card
-                            class="flex"
-                            flat
-                            tile
-                            min-height="122"
-                        >
+                                </v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </v-row>
+                <v-footer dark padless>
+                    <v-card class="flex" flat tile min-height="122">
                         <v-card-title class="teal">
                             <strong class="subheading">Zoon!</strong>
 
                             <v-spacer></v-spacer>
 
-                            <v-btn
-                                v-for="icon in icons"
-                                :key="icon"
-                                class="mx-4"
-                                dark
-                                icon
-                                to="/mypage"
-                            >
-                            <v-icon size="24px">
-                                {{ icon }}
-                            </v-icon>
+                            <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon to="/mypage">
+                                <v-icon size="24px">
+                                    {{ icon }}
+                                </v-icon>
                             </v-btn>
                         </v-card-title>
 
                         <v-card-text class="py-2 white--text text-center">
                             {{ new Date().getFullYear() }} — <strong>Zoon</strong>
                         </v-card-text>
-                        </v-card>
-                    </v-footer>
-                </v-flex>
-            </v-layout>
-        </v-container>
-    </content>                                  
+                    </v-card>
+                </v-footer>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</content>
 </template>
+
 <script>
-    export default {
-        data() {
-            return {
-                size:"display-2",
+export default {
+    data() {
+        return {
+            size: "display-2",
+        }
+    },
+    computed: {
+        user_id: {
+            get() {
+                return this.$store.getters.user_id
+            },
+            set(value) {
+                return this.$store.commit('set_user_id', value)
             }
         },
-        computed:{
-            user_id:{
-                get(){
-                    return this.$store.getters.user_id
-                },
-                set(value){
-                    return this.$store.commit('set_user_id',value)
-                }
-            },
-            user_fname(){
-                return this.$store.getters.user_fname
-            },
+        user_fname() {
+            return this.$store.getters.user_fname
         },
-        methods:{
-            logout(){
-                this.$store.commit('logout')
-            }
-        },
-        created() {
-            this.$store.commit('onAuthStateChanged')
-        },
-    }
+    },
+    methods: {
+        logout() {
+            this.$store.commit('logout')
+        }
+    },
+    created() {
+        this.$store.commit('onAuthStateChanged')
+    },
+}
 </script>
